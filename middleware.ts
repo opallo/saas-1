3b71618874e1373a15edde621e9266b7386f9d37
opallo <opallo.collins@gmail.com>
2025-08-18 01:11:54 -0700

    feat: add public routes

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,9 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
 const publicRoutes = createRouteMatcher([
-  
+  '/sign-in',
+  '/api/checkout',
+  '/api/webhook',
 ])
 
 export default clerkMiddleware((auth, req) => {
